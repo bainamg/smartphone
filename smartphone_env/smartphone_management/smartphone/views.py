f2d415e2d857bc6a1e405d34578685fb6c2f3462
bainamg <bainamrutha@gmail.com>
2023-11-17 12:34:29 +0530

    static page

--- smartphone_env/smartphone_management/smartphone/views.py
+++ smartphone_env/smartphone_management/smartphone/views.py
@@ -4,7 +4,7 @@ from . models import Brand, PhoneModels, Transactions
 from django.contrib.auth.models import User
 from . import views
 import traceback   
-from django.db.models import Max 
+from django.db.models import Max,Sum
 
 # Create your views here.
 def index(request):
@@ -81,14 +81,31 @@ def final(request, model_id):
 def print_statics(request):
     print("---- in ")
     try:
-        model_obj=PhoneModels.objects.all()
-        top_selling_model=PhoneModels.objects.filter().order_by('-item_sold').first()
-        return render(request,'static.html', {'model': top_selling_model})
+
+        top_brand=PhoneModels.objects.values('brand_id').annotate(sum_item_sold=Sum('item_sold')).order_by('-sum_item_sold')[0]['sum_item_sold']
+        brand_item_sold=PhoneModels.objects.values('brand_id').annotate(sum_item_sold=Sum('item_sold'))
+        top_brands=[each_brand for each_brand in brand_item_sold if each_brand['sum_item_sold']==10]
+        print(top_brands)
+        
+        brand_names=[]
+        for b in top_brands:
+            brand_obj=Brand.objects.filter(id=b['brand_id'])
+            
+
+
+            for name in brand_obj:
+                brand_names.append(name.Name)
+        
+        max_itemsold=PhoneModels.objects.filter().aggregate(Max('item_sold'))['item_sold__max']
+        top_selling_models=PhoneModels.objects.filter(item_sold=max_itemsold).order_by('name')
+        #print(top_selling_models)
+        return render(request,'static.html', {'model': top_selling_models,'b_names':brand_names})
     except Exception as e:
         message = traceback.format_exc()
         print(message)
         return render(request,'exception.html')
     
+
 
 
 
